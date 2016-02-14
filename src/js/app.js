396262790f756792b8c2ead3b70b8e70a2cf56a6
hyojun.lee <lgk5457@naver.com>
2016-02-14 14:30:24 +0900

    move react path from res to js

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@
 //import React from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TestButton from 'res/react/TestButton.js';
+import TestButton from 'js/react/TestButton.js';
 
 require('res/less/sb-admin-2.less');
 ReactDOM.render(<TestButton/> , document.getElementById('study_container'));
